85130cce3288f2d5469e01ccf5854388d8ef7608
vitorsniquer <vitorsniquer@gmail.com>
2022-10-31 15:55:46 -0300

    fix: update database in metricsutil

--- metricsutils.py
+++ metricsutils.py
@@ -40,39 +40,45 @@ def main():
     #     }
 
     database = {
-        "intents": [
-                {
-                    "tag": "welcome",
-                    "patterns": [],
-                    "responses": ["Olá, serei seu assistente virtual, em que posso te ajudar?","Salve, qual foi ?", "Manda pro pai, Lança a braba", "No que posso te ajudar ?"],
-                    "context": [""]
-                },
-                {
-                    "tag": "myclasses",
-                    "patterns": [],
-                    "responses": ["Entendi, você deseja saber suas salas","Você deseja saber suas salas ?", "Ah, você quer saber qual sala ? ", "Suas Aulas ?"],
-                    "context": [""]
-                },
-                {
-                    "tag": "businfo",
-                    "patterns": [],
-                    "responses": ["Fretados","Horarios Fretado"], #provisório
-                    "context": [""]
-                },
-                # {
-                #     "tag": "disc_info",
-                #     "patterns": ['Gostaria de saber da ementa da disciplina','ementa da materia','quero saber da ementa','quero saber do plano de ensino','quais os requsitos da materia','qual a bibliografia da disciplina'],
-                #     "responses": ['Informações da disciplina X','Para a disciplina Y, as informações são as seguintes'],
-                #     "context": [""]
-                # },
-                {
-                    "tag": "anything_else",
-                    "patterns": [],
-                    "responses": ["Desculpa, não entendi o que você falou, tente novamente!","Não compreendi a sua solicitação, talvez eu possa te ajudar"],
-                    "context": [""]
-                }
-            ]
-        }
+    "intents": [
+            {
+                "tag": "welcome",
+                "patterns": [],
+                "responses": ["Olá, serei seu assistente virtual, em que posso te ajudar?","Salve, qual foi ?", "Manda pro pai, Lança a braba", "No que posso te ajudar ?"],
+                "context": [""]
+            },
+            {
+                "tag": "myclasses",
+                "patterns": [],
+                "responses": ["Entendi, você deseja saber suas salas","Você deseja saber suas salas ?", "Ah, você quer saber qual sala ? ", "Suas Aulas ?"],
+                "context": [""]
+            },
+            {
+                "tag": "businfo",
+                "patterns": [],
+                "responses": ["Esses são os horários dos fretados","Horarios dos fretados: ", "Ah, você quer saber o horário dos fretados"],
+                "context": [""]
+            },
+            {
+                "tag": "discinfo",
+                "patterns": [],
+                "responses": ['Informações da disciplina X','Para a disciplina Y, as informações são as seguintes: '],
+                "context": [""]
+            },
+        {
+                "tag": "ru",
+                "patterns": [],
+                "responses": ['O cárdapio de hoje é esse:','Para o almoço temos:', 'Para o jantar teremos:'], # provisório
+                "context": [""]
+            },
+            {
+                "tag": "anything_else",
+                "patterns": [],
+                "responses": ["Desculpa, não entendi o que você falou, tente novamente!","Não compreendi a sua solicitação, talvez eu possa te ajudar", "Por favor, digite novamente"],
+                "context": [""]
+            }
+        ]
+    }
 
     database = gerador.fill_database(database,300)
     #print(database)
