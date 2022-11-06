d1ef7a2db9777f231ea5ca26bf7210cf5c9360fd
vitorsniquer <vitorsniquer@gmail.com>
2022-11-06 16:06:24 -0300

    feat: teste inicial da integracao do chatbot com o ETL
    Co-authored-by: HigorBraz <hygoorbraz@gmail.com>

--- tatuia.py
+++ tatuia.py
@@ -16,6 +16,9 @@ import pickle
 import json
 import os
 from unittest import result
+
+from src.load.DB import get_db,DBCollections
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 # tensorflow tags
 # 0 = all messages are logged (default behavior)
@@ -207,6 +210,27 @@ def main():
 
     #tatu_zap.eval_model()
     
+    turmas_por_ra_collection = get_db[DBCollections.TURMAS_POR_RA]
+
+    def turmas(RA):
+        QUERY = {"RA": str(RA)}
+        result = list(turmas_por_ra_collection.find(QUERY))
+        lista = result[0]["TURMAS"]
+        for res in lista:del res['_id']
+        lista_clean = [dict(item) for item in {tuple(dict.items()) for dict in lista}]
+        for disciplina in lista_clean:
+            teoria = disciplina['HORÁRIO TEORIA']
+            pratica = disciplina['HORÁRIO PRÁTICA']
+            nome = disciplina['DISCIPLINA - TURMA']
+
+            if teoria == 0:
+                print('Disciplina: {}, Horário Prática: {}'.format(nome,pratica)) 
+            if pratica == 0:
+                print('Disciplina: {}, Horário Teoria: {}'.format(nome,teoria)) 
+        return lista_clean
+
+
+
     print(">>> Demo da funcionalidade de reconhecimento de intenção do TatuBot.")
     print(">>> Inicialmente a I.A foi treinada com cinco intenções (welcome,myclasses,businfo,discinfo,ru).")
     print(">>> Envie uma mensagem para o TatuBot!")
@@ -217,14 +241,15 @@ def main():
             if intent == "myclasses":
                 user_ra = tatu_zap.message_utils.is_ra(user_message)
                 if user_ra:
-                    print("Tatu: Já estou processando as turmas para o ra {}.".format(user_ra))
+                    turmas(user_ra)
                 else:
                     while True:
                         print("Tatu: Você solicitou informações sobre suas turmas, agora insira seu ra!")
                         expected_ra = input('user: ')
                         user_ra = tatu_zap.message_utils.is_ra(expected_ra)
                         if user_ra:
-                            print("Tatu: Já estou processando as turmas para o ra {}.".format(user_ra))
+                            #print("Tatu: Já estou processando as turmas para o ra {}.".format(user_ra))
+                            turmas(user_ra)
                             break
             elif intent == "businfo":
                 user_localtime =  tatu_zap.message_utils.check_origin(user_message)
